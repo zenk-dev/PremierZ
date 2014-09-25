1e258ecbbc5f6d8c95d5846b1f4d310847958747
tsuka54 <y_tsukagoshi@zenk.co.jp>
2014-09-25 17:39:55 +0900

    9.25 Update

--- baukis/config/routes.rb
+++ baukis/config/routes.rb
@@ -16,7 +16,7 @@ Rails.application.routes.draw do
     get 'staff_members' => 'staff_members#index'
     get 'staff_members/:id' => 'staff_members#show'
     get 'staff_members/new' => 'staff_members#new'
-    get 'staff_members/:id/new' => 'staff_members#edit'
+    get 'staff_members/:id/edit' => 'staff_members#edit'
     post 'staff_members' => 'staff_members#create'
     patch 'staff_members/:id' => 'staff_members#update'
     delete 'staff_members/:id' => 'staff_members#destroy'
