8fdcb071e898ea3c82f0fa0f0158c129cef7291c
tsuka54 <y_tsukagoshi@zenk.co.jp>
2014-10-01 17:50:02 +0900

    10/1 Update

--- baukis/app/controllers/admin/top_controller.rb
+++ baukis/app/controllers/admin/top_controller.rb
@@ -1,9 +1,5 @@
 class Admin::TopController < Admin::Base
   def index
-      if current_administrator
-        render action: 'dashboard'
-      else
-        render action: 'index'
-      end
+    render action: 'index'
   end
 end
--- baukis/config/routes.rb
+++ baukis/config/routes.rb
@@ -1,87 +1,24 @@
 Rails.application.routes.draw do
-  namespace :staff, path: '' do
-    root 'top#index'
-    get 'login' => 'sessions#new', as: :login
-    post 'session' => 'sessions#create', as: :session
-    delete 'session' => 'sessions#destroy'
+  constraints host: 'baukis.example.com' do
+    namespace :staff, path: '' do
+      root 'top#index'
+      get 'login' => 'sessions#new', as: :login
+      post 'session' => 'sessions#create', as: :session
+      delete 'session' => 'sessions#destroy'
+    end
   end
-  
+
   namespace :admin do
     root 'top#index'
     get 'login' => 'sessions#new', as: :login
     post 'session' => 'sessions#create', as: :session
     delete 'session' => 'sessions#destroy'
-    resources :staff_members
-=begin
-    get 'staff_members' => 'staff_members#index'
-    get 'staff_members/:id' => 'staff_members#show'
-    get 'staff_members/new' => 'staff_members#new'
-    get 'staff_members/:id/edit' => 'staff_members#edit'
-    post 'staff_members' => 'staff_members#create'
-    patch 'staff_members/:id' => 'staff_members#update'
-    delete 'staff_members/:id' => 'staff_members#destroy'
-=end
   end
 
   namespace :customer do
     root 'top#index'
   end
 
-  root 'errors#routing_error'
-  get '*anything' => 'errors#routing_error'
-
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+  root 'errors#not_found'
+  get '*anything' => 'errors#not_found'
 end
