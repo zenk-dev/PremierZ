306d30647ec5c56a16db80fa69944994ccebfdc4
tsuka54 <y_tsukagoshi@zenk.co.jp>
2014-10-07 17:31:05 +0900

    10/7 Update

--- baukis/spec/experiments/string_spec.rb
+++ baukis/spec/experiments/string_spec.rb
@@ -1,20 +1,16 @@
-require 'spec_helper'
+require 'rails_helper'
 
 describe String do
   describe '#<<' do
-    xexample '文字列の追加' do
+    example '文字の追加' do
       s = "ABC"
       s << "D"
       expect(s.size).to eq(4)
     end
-    xexample 'nilの追加' do
+
+    example 'nilは追加できない' do
       s = "ABC"
-      s << nil
-      expect(s.size).to eq(4)
-    end
-    xexample 'nilは追加できない', :exception do
-      s = "ABC"
-      expect {s << nil}.to raise_error(TypeError)
+      expect { s << nil }.to raise_error(TypeError)
     end
   end
 end
